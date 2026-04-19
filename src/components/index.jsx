5d44269ee7e140f43faeefcef321c3dd63d0c066
ianurag100x <anurag100x@gmail.com>
2026-04-20 04:06:41 +0530

    fix path dock

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import Welcome from "./Welcome";
-import Dock from "./dock";
+import Dock from "./Dock";
 import WindowControls from "./WindowControls";
 import Home from "./Home";
 
